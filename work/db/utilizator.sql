d415417af372d676efe2f9548174758a4c9cdcd6
ignatescu_s <ignatescustefan@gmail.com>
2019-03-27 19:05:33 +0200

    populare tabela utilizatori

---
+++ work/db/utilizator.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO `utilizator` (`nume`, `prenume`, `email`, `parola`, `cnp`, `telefon`) VALUES
+('Ana', 'Maria', 'ana@maria.ro', '12312', '12', '09321'),
+('Voicu', 'Ioan', 'voicu@mail.com', 'voicu123', '1921312', '03213'),
+('Cojocaru', 'Vlad', 'vlad@mail.com', 'vlad1234', '19213131', '0741123'),
+('Vranciu', 'Vasile', 'vasi@mail.com', 'vasi1234', '193213', '094213'),
+('Manea', 'Cristi', 'cristi@mail.com', 'cristi123', '198321', '071331'),
+('Dan', 'Marius', 'dan@marius.ro', 'dan123', '23', '08732'),
+('Creangă', 'Mircea', 'mircea@mail.com', 'mircea1234', '23123', '083123'),
+('Ionescu', 'George', 'george@mail.com', 'george1234', '23432', '04123'),
+('Eminescu', 'Mihai', 'emi@mail.com', 'luceafarul1234', '291231', '0831234'),
+('Micle', 'Veronica', 'vero@mail.com', 'emi1234', '29123123', '039231');
