864f8ae9cde861ee2edb100f6722d8cde8bef0da
ignatescu_s <ignatescustefan@gmail.com>
2019-04-11 17:02:12 +0300

    code refactory + dashboardPanel actualizat(delatii cont)

--- work/backend/RIPBankDesktop/src/main/java/com/RIPBankDesktop/DashBoardRIP.java
+++ work/backend/RIPBankDesktop/src/main/java/com/RIPBankDesktop/DashBoardRIP.java
@@ -1,6 +1,8 @@
 package com.RIPBankDesktop;
 
 import java.awt.Color;
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
@@ -10,6 +12,12 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.SystemColor;
+import javax.swing.border.LineBorder;
+import javax.swing.UIManager;
+import javax.swing.JTextArea;
 
 public class DashBoardRIP extends JFrame {
 
@@ -18,84 +26,216 @@ public class DashBoardRIP extends JFrame {
 	 */
 	private static final long serialVersionUID = -7456512844086031995L;
 	private JPanel contentPane;
-	JLabel lblName = new JLabel("");
+	private LoginPanel loginPanel;
+	private JLabel lblName;
+	private JLabel bannerLabel;
+	private JPanel rightPanel;
+	private JButton btnSetariCont;
+	private JButton btnLogout;
+	private JLabel lblPrenume;
+	private JLabel lblEmail;
+	private JPanel employeePanel;
+	private JLabel employeeLbl;
+	private JLabel employeeNameLbl;
+	private Employee angajat;
+	private JButton btnModifica;
+	private JPanel mainPanel;
+	private JPanel firstPanel;
+	private JPanel panelWelc;
+	private JLabel lblBineAiVenit;
+	private JPanel panelContent;
+
+	/**
+	 * Launch the demo Board.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DashBoardRIP frame = new DashBoardRIP(new Employee("Aline", "Dua", "alina@mail.com"));
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
 	/**
 	 * Create the frame.
 	 */
-	public DashBoardRIP(final Employee angajat) {
-		
+	public DashBoardRIP(Employee employee) {
+
 		super("RIP Bank");
-		
-		System.out.println("dashboard angajat "+ angajat.toString());
-		//Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(0, 0, 1376, 738);
+
+		angajat = new Employee(employee.getNume(), employee.getPrenume(), employee.getEmail());
+		setIconImage(Toolkit.getDefaultToolkit().getImage(LoginPanel.class.getResource("/img/rip_icon.png")));
+		lblName = new JLabel("Nume");
+		bannerLabel = new JLabel("");
 		contentPane = new JPanel();
+		rightPanel = new JPanel();
+		btnSetariCont = new JButton("Detalii cont");
+		btnModifica = new JButton("Modifica");
+		btnModifica.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+				
+		btnSetariCont.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnSetariCont.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				if (btnSetariCont.getText().equals("Detalii cont")) {
+
+					lblName.setVisible(true);
+					lblPrenume.setVisible(true);
+					lblEmail.setVisible(true);
+					btnModifica.setVisible(true);
+					btnSetariCont.setText("Ascunde detalii");
+				} else {
+					lblName.setVisible(false);
+					lblPrenume.setVisible(false);
+					lblEmail.setVisible(false);
+					btnModifica.setVisible(false);
+					btnSetariCont.setText("Detalii cont");
+				}
+			}
+		});
+		btnLogout = new JButton("Logout");
+		btnLogout.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblPrenume = new JLabel("Prenume");
+		lblEmail = new JLabel("Email");
+		employeePanel = new JPanel();
+		employeeLbl = new JLabel("");
+		employeeNameLbl = new JLabel("", SwingConstants.CENTER);
+
+		lblName.setText(angajat.getNume());
+		lblPrenume.setText(angajat.getPrenume());
+		lblEmail.setText(angajat.getEmail());
+
+		System.out.println("dashboard angajat " + angajat.toString());
+		// Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 1384, 754);
+
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		//JLabel lblName = new JLabel("");
-		JLabel bannerLabel = new JLabel("");
+
 		bannerLabel.setIcon(new ImageIcon(DashBoardRIP.class.getResource("/img/banner.png")));
 		bannerLabel.setBounds(0, 0, 1200, 140);
 		contentPane.add(bannerLabel);
-		
-		JPanel rightPanel = new JPanel();
-		rightPanel.setBounds(0, 139, 200, 593);
+
+		rightPanel.setBounds(0, 139, 249, 576);
 		rightPanel.setBackground(new Color(21, 98, 114));
 		contentPane.add(rightPanel);
 		rightPanel.setLayout(null);
-		
-		JButton btnSetariCont = new JButton("Detalii cont");
-		btnSetariCont.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				lblName.setText(angajat.getNume());
-			}
-		});
-		btnSetariCont.setBounds(58, 49, 89, 23);
+
+		btnSetariCont.setBounds(69, 49, 110, 23);
 		rightPanel.add(btnSetariCont);
-		
-		JButton btnLogout = new JButton("Logout");
-		btnLogout.setVisible(false);
+
 		btnLogout.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				LoginPanel loginPanel=new LoginPanel();
+				loginPanel = new LoginPanel();
 				loginPanel.setVisible(true);
 				dispose();
 			}
 		});
-		btnLogout.setBounds(58, 494, 89, 23);
+		btnLogout.setBounds(69, 463, 110, 23);
 		rightPanel.add(btnLogout);
-		
-		
-		lblName.setBounds(10, 89, 180, 30);
+
+		lblName.setHorizontalAlignment(SwingConstants.CENTER);
+		lblName.setBounds(52, 97, 144, 29);
+		lblName.setVisible(false);
 		rightPanel.add(lblName);
+
+		lblPrenume.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPrenume.setBounds(52, 137, 144, 29);
+		lblPrenume.setVisible(false);
+		rightPanel.add(lblPrenume);
+
+		lblEmail.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEmail.setBounds(52, 177, 144, 29);
+		lblEmail.setVisible(false);
+		rightPanel.add(lblEmail);
 		
-		JLabel label = new JLabel("");
-		label.setBounds(10, 129, 180, 30);
-		rightPanel.add(label);
 		
-		JPanel employeePanel = new JPanel();
-		employeePanel.setBounds(1200, 0, 160, 140);
+		btnModifica.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnModifica.setBounds(69, 217, 110, 23);
+		btnModifica.setVisible(false);
+		rightPanel.add(btnModifica);
+
+		employeePanel.setBounds(1200, 0, 168, 140);
 		employeePanel.setBackground(new Color(251, 141, 2));
 		contentPane.add(employeePanel);
 		employeePanel.setLayout(null);
-		
-		JLabel employeeLbl = new JLabel("");
-		employeeLbl.setBounds(38, 11, 83, 83);
+
+		employeeLbl.setBounds(42, 11, 83, 83);
 		employeePanel.add(employeeLbl);
 		employeeLbl.setIcon(new ImageIcon(DashBoardRIP.class.getResource("/img/manager.png")));
-		
-		JLabel employeeNameLbl = new JLabel("",SwingConstants.CENTER);
-		employeeNameLbl.setBounds(10, 96, 140, 33);
+
+		employeeNameLbl.setBounds(14, 96, 140, 33);
 		employeePanel.add(employeeNameLbl);
-		employeeNameLbl.setText(angajat.getNume()+" "+angajat.getPrenume());
+		employeeNameLbl.setText(angajat.getNume() + " " + angajat.getPrenume());
 		
+		mainPanel = new JPanel();
+		mainPanel.setBackground(new Color(250,250,250));
+		mainPanel.setBounds(247, 139, 1121, 576);
+		contentPane.add(mainPanel);
+		mainPanel.setLayout(null);
+		
+		firstPanel = new JPanel();
+		firstPanel.setBorder(new LineBorder(new Color(105, 105, 105)));
+		firstPanel.setBackground(new Color(235, 238, 239));
+		firstPanel.setBounds(83, 27, 954, 521);
+		mainPanel.add(firstPanel);
+		firstPanel.setLayout(null);
+		
+		panelWelc = new JPanel();
+		panelWelc.setBorder(new LineBorder(UIManager.getColor("Button.disabledForeground")));
+		panelWelc.setBounds(374, 38, 400, 74);
+		panelWelc.setBackground(new Color(251, 141, 2));
+		firstPanel.add(panelWelc);
+		panelWelc.setLayout(null);
+		
+		lblBineAiVenit = new JLabel("Bine ai venit ");
+		lblBineAiVenit.setFont(new Font("Georgia", Font.BOLD | Font.ITALIC, 29));
+		lblBineAiVenit.setHorizontalAlignment(SwingConstants.CENTER);
+		lblBineAiVenit.setForeground(SystemColor.window);
+		lblBineAiVenit.setBounds(10, 11, 380, 53);
+		panelWelc.add(lblBineAiVenit);
+		
+		panelContent = new JPanel();
+		panelContent.setBorder(new LineBorder(UIManager.getColor("Button.darkShadow")));
+		panelContent.setBackground(UIManager.getColor("Button.highlight"));
+		panelContent.setBounds(68, 169, 818, 312);
+		firstPanel.add(panelContent);
+		panelContent.setLayout(null);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setToolTipText("username");
+		textArea.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		textArea.setBorder(new LineBorder(new Color(171, 173, 179)));
+		textArea.setBounds(316, 50, 186, 38);
+		panelContent.add(textArea);
+		
+		JLabel lblCautaClient = new JLabel("Cauta client");
+		lblCautaClient.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCautaClient.setBounds(193, 50, 91, 38);
+		panelContent.add(lblCautaClient);
+		
+		JButton btnCauta = new JButton("");
+		btnCauta.setIcon(new ImageIcon(DashBoardRIP.class.getResource("/img/client_search.png")));
+		btnCauta.setBounds(532, 50, 38, 38);
+		panelContent.add(btnCauta);
+
 	}
 
 	public boolean getThisResizable() {
 		return isResizable();
 	}
+
 	public void setThisResizable(boolean resizable) {
 		setResizable(resizable);
 	}
--- work/backend/RIPBankDesktop/src/main/java/com/RIPBankDesktop/LoginPanel.java
+++ work/backend/RIPBankDesktop/src/main/java/com/RIPBankDesktop/LoginPanel.java
@@ -38,9 +38,21 @@ public class LoginPanel extends JFrame {
 	private JPanel contentPane;
 	private JPasswordField txtPasswordField;
 	private Employee angajat;
+	private JTextArea txtUsername;
+	private JDesktopPane desktopPane;
+	private JLabel loginIcon;
+	private JLabel lblSignIn;
+	private JLabel lblIncorrect;
+	private JButton login;
+	private EmployeeDTO user;
+	private JLabel passwordIcon;
+	private JLabel userIcon;
+	private JButton btnCancel;
+	private JLabel lblForgotPass;
+	private JPanel panel;
 
 	/**
-	 * Launch the application.
+	 * Launch the Login application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -58,23 +70,23 @@ public class LoginPanel extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public static final String encrypt(String md5) {
-	    try {
-	        java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
-	        byte[] array = md.digest(md5.getBytes());
-	        StringBuffer sb = new StringBuffer();
-	        for (int i = 0; i < array.length; ++i) {
-	          sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
-	       }
-	        return sb.toString();
-	    } catch (java.security.NoSuchAlgorithmException e) {}
-	    return null;
-	}
+
 	public LoginPanel() {
-		
-		
+
 		super("Login");
-		
+
+		txtUsername = new JTextArea();
+		desktopPane = new JDesktopPane();
+		loginIcon = new JLabel();
+		lblSignIn = new JLabel("Sign in", SwingConstants.CENTER);
+		lblIncorrect = new JLabel("Incorrect credentials.", SwingConstants.LEFT);
+		login = new JButton("Login");
+		passwordIcon = new JLabel("");
+		userIcon = new JLabel("");
+		btnCancel = new JButton("Cancel");
+		lblForgotPass = new JLabel("Forgot Password?", SwingConstants.LEFT);
+		panel = new JPanel();
+
 		setIconImage(Toolkit.getDefaultToolkit().getImage(LoginPanel.class.getResource("/img/rip_icon.png")));
 		setResizable(false);
 		setBackground(Color.WHITE);
@@ -84,154 +96,133 @@ public class LoginPanel extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
-		JDesktopPane desktopPane = new JDesktopPane();
+
 		desktopPane.setVerifyInputWhenFocusTarget(false);
 		desktopPane.setToolTipText("");
 		desktopPane.setBackground(new java.awt.Color(235, 238, 239));
 		contentPane.add(desktopPane, BorderLayout.CENTER);
 		desktopPane.setLayout(null);
-		
-		final JTextArea txtUsername = new JTextArea();
+
 		txtUsername.setToolTipText("username");
 		txtUsername.setFont(new Font("SansSerif", Font.PLAIN, 14));
 		txtUsername.setBounds(127, 176, 136, 23);
 		txtUsername.setBorder(new LineBorder(new Color(171, 173, 179)));
 		desktopPane.add(txtUsername);
-		
-		JLabel loginIcon = new JLabel();
+
 		loginIcon.setBounds(0, 0, 391, 104);
 		loginIcon.setIcon(new ImageIcon(LoginPanel.class.getResource("/img/logn2.jpg")));
 		desktopPane.add(loginIcon);
-		
-		JLabel lblSignIn = new JLabel("Sign in",SwingConstants.CENTER);
+
 		lblSignIn.setBounds(127, 127, 136, 38);
 		desktopPane.add(lblSignIn);
 		lblSignIn.setFont(new Font("Segoe UI", Font.BOLD, 28));
 		lblSignIn.setForeground(new Color(0, 0, 102));
-		
+
 		txtPasswordField = new JPasswordField();
 		txtPasswordField.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		txtPasswordField.setBorder(new LineBorder(new Color(171, 173, 179)));
 		txtPasswordField.setBounds(127, 217, 136, 23);
 		desktopPane.add(txtPasswordField);
-		
-		
-		
-		final JLabel lblIncorrect = new JLabel("Incorrect credentials.", SwingConstants.LEFT);
-		JButton login = new JButton("Login");
+
+		lblIncorrect = new JLabel("Incorrect credentials.", SwingConstants.LEFT);
 		login.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
-				//GraphicsEnvironment localEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
-				//GraphicsDevice defaultDevice = localEnvironment.getDefaultScreenDevice();
-				//.setFullScreenWindow(window);
-				
-				//TODO: logica de login
-				
-	
-				String userString=txtUsername.getText();
-				char[] passwordString=txtPasswordField.getPassword();
-				
-				//char[] pass = tb_pwd.getPassword();
+
+				String userString = txtUsername.getText();
+				char[] passwordString = txtPasswordField.getPassword();
+
 				String final_pass = "";
 				for (char x : passwordString) {
-				    final_pass += x;
+					final_pass += x;
 				}
-				
-				System.out.println(userString+" "+ final_pass);
-				
-				EmployeeDTO user= new EmployeeDTO(userString, final_pass);
-				
-				//send request
-				
-				Manager m=new Manager();
-				
-				Response myResponse=m.createJsonUtilizator(user);
-				
+
+				System.out.println(userString + " " + final_pass);
+
+				user = new EmployeeDTO(userString, final_pass);
+
+				// send request
+
+				Manager m = new Manager();
+
+				Response myResponse = m.createJsonUtilizator(user);
+
 				System.out.println("My response");
 				System.out.println(myResponse);
-				
+
 				String responseAsString = myResponse.readEntity(String.class);
 				JSONObject jsonObject = new JSONObject(responseAsString);
-				
-				
-				int loginOk=jsonObject.getInt("LoginOk");
+
+				int loginOk = jsonObject.getInt("LoginOk");
 				System.out.println(loginOk);
-				
+
 				System.out.println("My response");
 				System.out.println(myResponse);
-				
-				if(loginOk==0) {
-					//false
-					//set label date incorecte
-					System.out.println("Date incorecte : loginOk="+loginOk);
-					lblIncorrect.setVisible(true);;
-				}
-				else{
-					
-					System.out.println("Date valide : loginOk="+loginOk);
-					
-					String numeString=jsonObject.getString("Nume");
-					String prenumeString=jsonObject.getString("Prenume");
-					String emailString=jsonObject.getString("Email");
-					
-					angajat=new Employee(numeString,prenumeString,emailString);
-					
-					DashBoardRIP window=new DashBoardRIP(angajat);
-					
-					System.out.println("loginpanel angajat "+ angajat.toString());
+
+				if (loginOk == 0) {
+					// false
+					// set label date incorecte
+					System.out.println("Date incorecte : loginOk=" + loginOk);
+					lblIncorrect.setVisible(true);
+					;
+				} else {
+
+					System.out.println("Date valide : loginOk=" + loginOk);
+
+					String numeString = jsonObject.getString("Nume");
+					String prenumeString = jsonObject.getString("Prenume");
+					String emailString = jsonObject.getString("Email");
+
+					angajat = new Employee(numeString, prenumeString, emailString);
+
+					DashBoardRIP window = new DashBoardRIP(angajat);
+
+					System.out.println("loginpanel angajat " + angajat.toString());
 					window.setVisible(true);
-		            dispose();
+					dispose();
 				}
-				}
-				
+			}
+
 		});
 		login.setBorder(null);
 		login.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
 		login.setForeground(SystemColor.textHighlightText);
-		//login.setBorder(new LineBorder(SystemColor.scrollbar, 1, true));
 		login.setBackground(new Color(87, 184, 70));
 		login.setBounds(203, 287, 60, 23);
+
 		desktopPane.add(login);
-		
-		JLabel passwordIcon = new JLabel("");
+
 		passwordIcon.setIcon(new ImageIcon(LoginPanel.class.getResource("/img/key.png")));
 		passwordIcon.setBounds(103, 217, 23, 23);
 		desktopPane.add(passwordIcon);
-		
-		JLabel userIcon = new JLabel("");
+
 		userIcon.setIcon(new ImageIcon(LoginPanel.class.getResource("/img/user.png")));
 		userIcon.setBounds(103, 176, 23, 23);
 		desktopPane.add(userIcon);
-		
-		JButton btnCancel = new JButton("Cancel");
+
 		btnCancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
 		});
-		
+
 		btnCancel.setForeground(Color.WHITE);
 		btnCancel.setBorder(null);
 		btnCancel.setBackground(new Color(87, 184, 70));
 		btnCancel.setBounds(127, 287, 60, 23);
 		desktopPane.add(btnCancel);
-		
-		JLabel lblForgotPass = new JLabel("Forgot Password?",SwingConstants.LEFT);
+
 		lblForgotPass.setBounds(127, 262, 136, 14);
 		lblForgotPass.setFont(new Font("SansSerif", Font.PLAIN, 10));
 		lblForgotPass.setForeground(new Color(87, 184, 70));
 		desktopPane.add(lblForgotPass);
-		
+
 		lblIncorrect.setVisible(false);
 		lblIncorrect.setForeground(Color.RED);
 		lblIncorrect.setFont(new Font("SansSerif", Font.PLAIN, 10));
 		lblIncorrect.setBounds(127, 247, 136, 14);
 		desktopPane.add(lblIncorrect);
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(250,250,250));
+
+		panel.setBackground(new Color(250, 250, 250));
 		panel.setBounds(86, 115, 219, 214);
 		desktopPane.add(panel);
 	}
